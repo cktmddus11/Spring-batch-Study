0123d691bffa3dcaa65c7ef46ff2d74779650701
syCha <sy.cha@quintet.co.kr>
2020-05-22 10:14:30 +0900

    commit

--- batch-processing2/src/main/java/exam/StepNextConditionalJobConfiguration.java
+++ batch-processing2/src/main/java/exam/StepNextConditionalJobConfiguration.java
@@ -1,9 +1,13 @@
 package exam;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +16,9 @@ import org.springframework.context.annotation.Configuration;
 public class StepNextConditionalJobConfiguration {
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
+	
+	private static Logger log = LoggerFactory.getLogger(StepNextConditionalJobConfiguration.class);
+	
 	@Autowired
 	public StepNextConditionalJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
 		this.jobBuilderFactory = jobBuilderFactory;
@@ -21,31 +28,45 @@ public class StepNextConditionalJobConfiguration {
 	public Job stepNextConditionalJob(){
 		return jobBuilderFactory.get("stepNextConditionalJob")
 				.start(conditionalJobStep1())
-					.on("FAILED")
+					.on("FAILED") // 캐치할 ExitStatus
 					.to(conditionalJobStep3())
 					.on("*")
-					.end()
-				.from(conditionalJobStep1())
+					.end() // flowbuilder 반환
+				.from(conditionalJobStep1()) // step1의 추가 이벤트
 					.on("*")
 					.to(conditionalJobStep2())
 					.next(conditionalJobStep3())
 					.on("*")
 					.end()
-				.end()
+				.end() // flowbuilder 종료
 				.build();
 				
 	}
 	private Step conditionalJobStep3() {
-		// TODO Auto-generated method stub
-		return null;
+		return stepBuilderFactory.get("conditionalJobStep3")
+				.tasklet((contribute, chukContext) -> {
+					log.info("=====step3=======");
+					return RepeatStatus.FINISHED;
+				})
+				.build();
 	}
 	private Step conditionalJobStep2() {
-		// TODO Auto-generated method stub
-		return null;
+		return stepBuilderFactory.get("conditionalJobStep2")
+				.tasklet((contribute, chukContext) -> {
+					log.info("=====step2=======");
+					return RepeatStatus.FINISHED;
+				})
+				.build();
 	}
+
 	private Step conditionalJobStep1() {
-		// TODO Auto-generated method stub
-		return null;
+		return stepBuilderFactory.get("conditionalJobStep1")
+				.tasklet((contribute, chukContext) -> {
+					log.info("=====step1=======");
+					//contribute.setExitStatus(ExitStatus.FAILED); // exitStatus failed로 지정
+					return RepeatStatus.FINISHED;
+				})
+				.build();
 	}
 	
 	
